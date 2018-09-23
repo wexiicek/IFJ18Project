b62e3e8702065527d9cc7cbab82a0ec76463a23e
Dominik <uJexxi@gmail.com>
2018-09-23 16:51:39 +0200

    Building the basics.
    Adding main.c file

---
+++ main.c
@@ -0,0 +1,16 @@
+/*BASIC INCLUDES*/
+	#include <stdio.h>
+	#include <stdlib.h>
+/*OPTIONAL INCLUDES*/
+	#include <string.h>
+	#include <math.h>
+/*END OF INCLUDES*/
+
+/*
+	Base main file for IFJ18 project.
+*/
+
+int main( int argc, const char* argv[] )
+{
+	
+}
