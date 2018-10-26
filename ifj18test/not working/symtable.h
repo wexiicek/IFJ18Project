685e534676acd9977285315c42f2ebeba14c4374
MajoPuki <majopuki007@gmail.com>
2018-10-26 22:43:40 +0200

    Update ifj18test/not working/symtable.h

--- ifj18test/not working/symtable.h
+++ ifj18test/not working/symtable.h
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 
 #include "string.h"
+#include "errors.h"
 
 #ifndef IFJ_SYMTABLE_H
 #define IFJ_SYMTABLE_H
@@ -30,5 +31,31 @@ tBSTNodePtr BSTSearch (tBSTNodePtr, char*);
 void BSTInsert (tBSTNodePtr *, char*, void*, tNodeDataType);
 void BSTDelete (tBSTNodePtr *, char*);
 void BSTDispose(tBSTNodePtr *);
+/*------------------------------------------------- FUNCTIONS FOR SYMTABLE --------------------------------------------------*/
+
+typedef struct variable {
+    int dataType; /* sInteger, sFloat, sString */
+} tDataVariable;
+
+typedef struct function {
+    int returnDataType; /* sInteger, sFloat, sString */
+    bool declared;
+    bool defined;
+    string parameters;   
+    string paramName[10];
+} tDataFunction;
+
+typedef struct symtable {
+    tBSTNodePtr root;
+} tSymtable;
+
+
+void symTableInit(tSymtable*);
+tBSTNodePtr symTableSearch(tSymtable*, string);
+void symTableDelete(tSymtable*, string);
+void symTableDispose(tSymtable*);
+void symTableInsertFunction(tSymtable*, string);
+void symTableInsertVariable(tSymtable*, string);
+
 
 #endif
