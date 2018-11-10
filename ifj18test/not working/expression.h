9e0e5dc4cfcd6442df8fed0260dac48da4eacc91
MajoPuki <majopuki007@gmail.com>
2018-11-10 17:48:26 +0100

    Update ifj18test/not working/expression.h

--- ifj18test/not working/expression.h
+++ ifj18test/not working/expression.h
@@ -10,20 +10,20 @@
  */
 typedef enum
 {
-	NT_EQ_NT,		// E -> E = E
-	NT_NEQ_NT,		// E -> E <> E
-	NT_LEQ_NT,		// E -> E <= E
-	NT_LTN_NT,		// E -> E < E
-	NT_MEQ_NT,		// E -> E => E
-	NT_MTN_NT,		// E -> E > E
-	NT_PLUS_NT,		// E -> E + E
-	NT_MINUS_NT,	// E -> E - E
-	NT_IDIV_NT,		// E -> E \ E
-	NT_MUL_NT,		// E -> E * E
-	NT_DIV_NT,		// E -> E / E
-	LBR_NT_RBR,		// E -> (E)
-	OPERAND,		// E -> i
-	NOT_A_RULE		// rule doesn't exist
+	PLUS_RULE,		    	// E -> E + E
+	MINUS_RULE,	        	// E -> E - E
+	//IDIV_RULE,			// E -> E \ E
+	MUL_RULE,				// E -> E * E
+	DIV_RULE,				// E -> E / E
+	EQUAL_RULE,				// E -> E = E
+	NOT_EQUAL_RULE,			// E -> E <> E
+	LESS_OR_EQUAL_RULE,		// E -> E <= E
+	LESS_RULE,				// E -> E < E
+	GREATHER_OR_EQUAL_RULE,	// E -> E => E
+	GREATHER_RULE,			// E -> E > E
+	BRACKETS_RULE,			// E -> (E)
+	OPERAND_RULE,			// E -> i
+	NOT_A_RULE				// rule doesn't exist
 } precAnalysisRules;
 
 /**
@@ -31,27 +31,27 @@ typedef enum
  */
 typedef enum
 {
-	PLUS,			// +
-	MINUS,			// -
-	MUL,			// *
-	DIV,			// /
-	//IDIV,			// \ /    //TODO Nevim, ci to tam mame
-	ASSIGN          // =
-    EQUAL,	        // ==
-	NOT_EQUAL,		// !=
-	LESS_OR_EQUAL,	// <=
-	LESS,			// <
-	MORE_OR_EQUAL,	// >=
-	MORE,			// >
-	LEFT_BRACKET,	// (
-	RIGHT_BRACKET,	// )
-	IDENTIFIER,		// ID
-	INT_NUMBER,		// int
-	FLOAT_NUMBER,	// float
-	STRING,			// string
-	DOLLAR,			// $
-	STOP,			// stop symbol used when reducing
-	NON_TERMINAL	// non-terminal
+	PLUS,				// +
+	MINUS,				// -
+	MUL,				// *
+	DIV,				// /
+	//IDIV,				// \ /    //TODO Nevim, ci to tam mame
+	ASSIGN          	// =
+    EQUAL,	        	// ==
+	NOT_EQUAL,			// !=
+	LESS_OR_EQUAL,		// <=
+	LESS,				// <
+	GREATHER_OR_EQUAL,	// >=
+	GREATHER,			// >
+	LEFT_BRACKET,		// (
+	RIGHT_BRACKET,		// )
+	IDENTIFIER,			// ID
+	INT_NUMBER,			// int
+	FLOAT_NUMBER,		// float
+	STRING,				// string
+	DOLLAR,				// $
+	STOP,				// stop symbol used when reducing
+	NON_TERMINAL		// non-terminal
 }precAnalysisTableSymbol;
 
 
