2853542414ec9f5d70974082555d2d3f9d7849a8
MajoPuki <majopuki007@gmail.com>
2018-11-10 17:48:29 +0100

    Update ifj18test/not working/stack.h

--- ifj18test/not working/stack.h
+++ ifj18test/not working/stack.h
@@ -1,45 +1,5 @@
 #include <stdbool.h>
-//#include "symtable.h"
-
-/**
- * Toto sem iba ujebal od Harmima.
- * @enum Symbols used for precednece analysis.
- */
-typedef enum
-{
-	PLUS,			/// +
-	MINUS,			/// -
-	MUL,			/// *
-	DIV,			/// /
-	IDIV,			/// \ /
-	EQ,				/// =
-	NEQ,			/// <>
-	LEQ,			/// <=
-	LTN,			/// <
-	MEQ,			/// >=
-	MTN,			/// >
-	LEFT_BRACKET,	/// (
-	RIGHT_BRACKET,	/// )
-	IDENTIFIER,		/// ID
-	INT_NUMBER,		/// int
-	DOUBLE_NUMBER,	/// double
-	STRING,			/// string
-	DOLLAR,			/// $
-	STOP,			/// stop symbol used when reducing
-	NON_TERM		/// non-terminal
-} Prec_table_symbol_enum;
-
-/**
- * @enum Data types.
- */
-typedef enum
-{
-	TYPE_UNDEFINED,	/// Data type undefined
-	TYPE_INT,		/// Integer data type
-	TYPE_DOUBLE,	/// Double data type
-	TYPE_STRING,	/// String data type
-	TYPE_BOOL,		/// Bool data type (actaully it's kinda imaginary)
-} Data_type;
+#include "symtable.h"
 
 /**
  * Taktez iba ujebane od Harmima.
@@ -47,13 +7,13 @@ typedef enum
  */
 typedef struct item
 {
-	Prec_table_symbol_enum symbol; // U Harmima v expression.h
+	precAnalysisTableSymbol symbol; 
 	Data_type data_type; // U Harmima v symtable.h
 	struct item *nextPtr; /// Pointer to next stack item.
 } StackItem;
 
 typedef struct {
-	StackItem* topPtr;
+	StackItem  *topPtr;
 } Stack;
 
 void stackInit(Stack* stack);
